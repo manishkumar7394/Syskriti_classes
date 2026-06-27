0209257535867838ebb39a3d087b29a0a25bb6e3
Manish Kumar <manishkumarg7394@gmail.com>
2026-06-27 12:41:12 +0530

    Update Window_function.sql

--- Flipkart_DB_using_SQL/Window_function.sql
+++ Flipkart_DB_using_SQL/Window_function.sql
@@ -36,3 +36,41 @@ select
 	lead(rating) over (order by listing_date) as lead_rating
 
 from flipkart
+-- 74. Rank products within each category.
+select
+	product_name,
+	category,
+	price,
+	rank() over (partition by category order by price) as rnk_products
+From flipkart
+-- 75. Calculate average category price using window functions.
+select 
+	product_name,
+	category, 
+	price,
+	round(
+	avg(price) over (
+	partition by category),2) as avg_category_price
+from flipkart
+
+-- 76. Calculate cumulative units sold by seller.
+select 
+	product_name,
+	seller, 
+	units_sold,
+	sum(units_sold) over (
+	partition by seller 
+	order by listing_date) as comulative_units_sold
+from flipkart
+
+-- 77. Find category-wise running revenue.
+select
+	product_name,
+	category,
+	listing_date,
+	price,
+	sum(price) over (
+	partition by category
+	order by listing_date, product_id) as running_revenue
+
+from flipkart
